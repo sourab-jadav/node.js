04961cdecb8996ad9099db083979ddbb8ee04495
sourab <sourab@sourab.sourab.com>
2025-09-21 22:26:55 +0530

    process.stdin
    .setEncoding
    .on('data',(data)=>{})
    process.exit(0);

--- 2process.js
+++ 2process.js
@@ -7,3 +7,14 @@ const process = require('process')
 
 // streams on process object - these control the communication channel between node process and os exec environment
 // stdin stdout stderr
+
+process.stdin.setEncoding('utf-8')
+console.log('type something and press enter')
+
+process.stdin.on('data',(data)=>{   // this is async
+    console.log('you typed :',data.trim());
+    if(data.trim()=='exit'){
+        process.exit(0);
+    }
+})
+
