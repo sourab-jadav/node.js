f9ad6496c45d5458cbe47e0c2598f088d1a5c459
sourab <sourab@sourab.sourab.com>
2025-09-21 15:09:28 +0530

    using setTimeout and clearTimeout

--- 1createservernode.js
+++ 1createservernode.js
@@ -7,3 +7,11 @@ const server = http.createServer((req,res)=>{
 server.listen(4242,()=>{
     console.log('server running at ',4242);
 });
+
+using setTimeout and clearTimeout
+
+let timerId=setTimeout(()=>{
+    console.log('timeout fired after 2 sec')
+},2000)
+
+clearTimeout(timerId)
