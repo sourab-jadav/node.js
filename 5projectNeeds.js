d610179a86fafeb284a9ac988fe39bc5388867e7
sourab <sourab@sourab.sourab.com>
2025-09-23 22:14:36 +0530

    project need : serving html [ express ]

--- 5projectNeeds.js
+++ 5projectNeeds.js
@@ -18,3 +18,14 @@
 // })
 
 
+// serving the static content
+ // * serving the html 
+ // * usage:- app.use(express.static(path.join(__dirname,'/public/')))
+ // * 
+ // * the above line of code tells start searching from the location from the directory where the file
+ // * is started running from there look into a folder called public and if came across any file with name
+ // * index.html 
+ // * then serve that file as response from the server
+ // * 
+ // * NOTE:- if the filename is index1.html then it will not serve 
+
