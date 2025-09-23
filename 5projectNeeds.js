171e23b62390b02a7c76a0dfac78eb5b04cc2970
sourab <sourab@sourab.sourab.com>
2025-09-23 21:57:03 +0530

    project need : for getting debug information [ express ]
    const morgan = require('morgan')
    
    app.use(morgan('combined'))
    app.use(morgan('tiny'))

---
+++ 5projectNeeds.js
@@ -0,0 +1,20 @@
+
+
+// for getting debug information we use morgan
+// const express=require('express')
+// const app=express()
+// const morgan=require('morgan')
+
+// app.use(morgan('combined'))  //this gives a lot of information
+// app.use(morgan('tiny'))  //this gives a tiny of information
+
+
+// app.get('/',(req,res)=>{
+//     res.send('welcome to express')
+// })
+
+// app.listen(300,()=>{
+//     console.log('servering running on port 300');
+// })
+
+
