bddfb59901a078fdc28230c15c228a28faa6fa85
sourab <sourab@sourab.sourab.com>
2025-09-23 22:08:39 +0530

    core_modules: path.join()

--- 3path.js
+++ 3path.js
@@ -7,3 +7,7 @@ console.log(path.resolve(process.env.HOME,'file.txt')) // /home/sourab/file.txt
 
 const parsedPath = path.parse('/home/sourab/file.txt') // creates an object out of path
 const formatPath = path.format(parsedPath)  // opposite of path.parse
+
+let new_path=path.join(__dirname,'/public') // path to current directory in which the file is present followed by public
+console.log(new_path)
+
