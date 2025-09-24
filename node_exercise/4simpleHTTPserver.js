d0f52e95f6fae1a62d9fd62566e09088e859f0db
sourab <sourab@sourab.sourab.com>
2025-09-24 06:07:40 +0530

    next step : parse the incoming json to js object

--- node_exercise/4simpleHTTPserver.js
+++ node_exercise/4simpleHTTPserver.js
@@ -1,9 +1,32 @@
 const http = require('http')
 
+
+let users =[
+    { id: 1, name: "Alice", email: "alice@test.com" },
+    { id: 2, name: "Bob", email: "bob@test.com" }
+]
+
+function getRequestBody(req){
+    return new Promise((resolve,reject)=>{
+        let body = "";
+        req.on("data",chunk=>{
+            body += chunk.toString();
+        })
+        req.on("end",()=>{
+            try{
+                resolve(body ? JSON.parse(body) : {});
+            }catch(err){
+                reject(err)
+            }
+        })
+    })
+}
+
+
 const server = http.createServer(async (req,res)=>{
-    // res.writeHead(200,{'Content-Type':'application/json'})
-    res.writeHead(200,{'Content-Type':'text/plain'})
-    res.setHeader("Content-Type", "application/json");
+    res.writeHead(200,{'Content-Type':'application/json'})
+    // res.writeHead(200,{'Content-Type':'text/plain'})
+    // res.setHeader("Content-Type", "application/json");
     const urlParts=req.url.split("/").filter(Boolean);
     res.end('hello world')
 })
