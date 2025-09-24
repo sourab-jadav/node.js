8b1af89fc8f0f891d0799a1f55bbb69b90257019
sourab <sourab@sourab.sourab.com>
2025-09-24 05:53:33 +0530

    interview: parsing the url safely in node.js

--- node_exercise/4simpleHTTPserver.js
+++ node_exercise/4simpleHTTPserver.js
@@ -1,8 +1,10 @@
 const http = require('http')
 
-const server = http.createServer((req,res)=>{
+const server = http.createServer(async (req,res)=>{
     // res.writeHead(200,{'Content-Type':'application/json'})
     res.writeHead(200,{'Content-Type':'text/plain'})
+    res.setHeader("Content-Type", "application/json");
+    const urlParts=req.url.split("/").filter(Boolean);
     res.end('hello world')
 })
 
