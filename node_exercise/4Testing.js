50843cf1d34e2260a7f369eddd79245dff982edc
sourab <sourab@sourab.sourab.com>
2025-09-24 06:44:23 +0530

    next: testing the api using curl
    -X <method_name>
    -H "Content-Type: application/json"
    -d '{"name":"charlie","email":"charlie@test.com"}'

---
+++ node_exercise/4Testing.js
@@ -0,0 +1,12 @@
+//using the curl command
+// # Get all users
+// curl http://localhost:3000/users
+
+// # Create user
+// curl -X POST http://localhost:3000/users -H "Content-Type: application/json" -d '{"name":"Charlie","email":"charlie@test.com"}'
+
+// # Update user
+// curl -X PUT http://localhost:3000/users/1 -H "Content-Type: application/json" -d '{"name":"Updated Alice"}'
+
+// # Delete user
+// curl -X DELETE http://localhost:3000/users/2
